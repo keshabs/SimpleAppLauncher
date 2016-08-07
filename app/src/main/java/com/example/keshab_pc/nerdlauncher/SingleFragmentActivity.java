af6197039fcddd81d0c616555c7fa064320dff65
Keshab Sharma <keshab95@gmail.com>
2016-08-07 11:02:53 -0700

    Initial commit

---
+++ app/src/main/java/com/example/keshab_pc/nerdlauncher/SingleFragmentActivity.java
@@ -0,0 +1,28 @@
+package com.example.keshab_pc.nerdlauncher;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Keshab-PC on 8/2/2016.
+ */
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+
+    protected abstract Fragment createFragment();
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+
+        FragmentManager fm = getSupportFragmentManager();
+        android.support.v4.app.Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if(fragment == null){
+            fragment = createFragment();
+            fm.beginTransaction().add(R.id.fragment_container,fragment).commit();
+        }
+    }
+}
